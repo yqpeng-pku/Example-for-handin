499c94d28df35d19c5f7e94dc5624350d24931ad
Yongqian Peng <yqpeng@stu.pku.edu.cn>
2024-09-13 16:02:43 +0800

    first push

---
+++ README.md
@@ -0,0 +1,25 @@
+# Module x:
+
+Practice Assignment
+
+
+## Installation
+
+To install and set up, follow the steps below:
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/xxx/xxx.git
+   ```
+
+2. Install the required dependencies (python=3.x):  
+    ```
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Run the code:
+    ```
+    python main.py
+    ```
---
+++ main.py
@@ -0,0 +1,7 @@
+print('Welcome to Directed Research in AI System (I)!')
+print('We hope you enjoy this exciting journey.')
+print('Throughout the course, you will explore diverse domains within Artificial Intelligence.')
+print('You will participate in hands-on projects and conduct independent research.')
+print('By the end, you will have honed essential research skills and gained valuable experience.')
+print('Stay curious, work diligently, and embrace the challenges that lie ahead.')
+print('Good luck, and have a great time!')
